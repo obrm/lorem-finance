8eb979a8beb8adbda87082753f4ef3285f11f47d
obrm <obrm770@gmail.com>
2021-03-09 15:06:56 +0200

    Added process.php for form submition

---
+++ process.php
@@ -0,0 +1,15 @@
+<?php
+  if (isset($_POST['email']))  {
+  
+    //Email information
+    $admin_email = "obrm770@gmail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    
+    //send email
+    mail($admin_email, "טופס חדש מאתר לורם פיננסים", $message . ' - ' . $phone, "מ:" . $email);
+    
+    header('Location: https://obrm.github.io/flex-website/success.html');
+  }
